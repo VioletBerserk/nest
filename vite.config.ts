371bae22571bcd3976662854e5c393fe76e2fcbe
VioletBerserk <vltxbrsrk@gmail.com>
2026-06-30 04:07:57 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nest/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
